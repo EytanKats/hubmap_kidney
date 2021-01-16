5ca1ebef6df27c05f7da709bd649a5f0f74b27f1
Eytan Kats <eytan.kats@gmail.com>
2021-01-16 12:11:15 +0200

    add saturation check in 'create_inference_dataset' script

--- create_inference_dataset.py
+++ create_inference_dataset.py
@@ -12,6 +12,8 @@ tiles_dir = "../../Datasets/HuBMAP_Kidney/test"
 tile_size = 1000
 tile_step = 1000
 resize_shape = (256, 256)
+sat_thr = 40
+num_pixels_thr = 10000
 
 # Create inference dataset
 inference_images_paths = glob.glob(inference_images_template)
@@ -29,9 +31,13 @@ for inference_image_path in inference_images_paths:
     for x in range(0, image.shape[1] - tile_size, tile_step):
         for y in range(0, image.shape[0] - tile_size, tile_step):
 
-            output_path_image = os.path.join(tiles_dir, image_name + "_" + str(x) + "_" + str(y) + "_" + ".png")
             tile_image = image[y: y + tile_size, x: x + tile_size]
-            cv2.resize(tile_image, resize_shape, interpolation=cv2.INTER_LINEAR)
-            cv2.imwrite(output_path_image, tile_image)
+            hsv_tile_image = cv2.cvtColor(tile_image, cv2.COLOR_BGR2HSV)
+            h, s, v = cv2.split(hsv_tile_image)
+            if (s > sat_thr).sum() > num_pixels_thr:
+
+                output_path_image = os.path.join(tiles_dir, image_name + "_" + str(x) + "_" + str(y) + "_" + ".png")
+                cv2.resize(tile_image, resize_shape, interpolation=cv2.INTER_LINEAR)
+                cv2.imwrite(output_path_image, tile_image)
 
 print("End of script")
